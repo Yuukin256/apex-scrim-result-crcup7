3b71b880ae65db0122edb404af38da5535843124
Yuukin256 <Yuukin256@gmail.com>
2021-10-04 22:28:18 +0900

    feat: update raw.tsx

--- src/pages/raw.tsx
+++ src/pages/raw.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
-import data from 'data/result.json';
+import Layout from 'components/layouts/layout';
+import Seo from 'components/layouts/seo';
+import result from 'data/result.json';
+import team from 'data/team.json';
 
 const RawDataPage: React.VFC = () => {
-  return <pre>{JSON.stringify(data, null, '\t')}</pre>;
+  const preStyle: React.CSSProperties = {
+    fontSize: '0.8rem',
+  };
+  return (
+    <Layout>
+      <Seo title="Home" />
+      <h2>team.json</h2>
+      <a href="https://raw.githubusercontent.com/Yuukin256/apex-scrim-result-crcup7/main/src/data/team.json">GitHub</a>
+      <pre style={preStyle}>{JSON.stringify(team, null, '  ')}</pre>
+      <h2>result.json</h2>
+      <a href="https://raw.githubusercontent.com/Yuukin256/apex-scrim-result-crcup7/main/src/data/result.json">
+        GitHub
+      </a>
+      <pre style={preStyle}>{JSON.stringify(result, null, '  ')}</pre>
+    </Layout>
+  );
 };
 
 export default RawDataPage;
